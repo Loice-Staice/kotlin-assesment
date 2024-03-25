5a5feca19a93ae92719bcae739591925d5660c6e
Loice-Staice <staiceloicea@gmail.com>
2024-03-25 09:20:25 +0300

    first commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,17 +1,20 @@
 import kotlin.math.pow
 
 fun main(){
-    val inputString = "Barnie bakes brown bagels and buns"
-    val words = inputString.split("Barnie bakes brown bagels and buns")
-    println(inputString)
+    count("Barnie bakes brown bagels and buns")
     println(4.0/3.0)
+    isPalindrome("akirachix")
 
+}
+fun count(sentence:String){
+    val words = sentence.split("b")
+    println(words)
 }
 fun calculate(radius: Double): Double {
     val pi = 3.14159
     return (4.0 / 3.0) * pi * radius.pow(3)
 }
-fun isPalindrome(str: String): Boolean {
-    val reversedStr = str.reversed()
-    return str == reversedStr
+fun isPalindrome(word: String): Boolean {
+    val r = word.reversed()
+    return word == word.reversed()
 }
